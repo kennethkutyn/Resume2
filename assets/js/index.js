d3c4843eb9593ac23b33633933f36f169d25e7f8
Kenneth Kutyn <kennethkutyn@gmail.com>
2023-06-08 13:57:42 +0700

    working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,33 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+
+
+
+const context = {
+  kind: 'user',
+  key: 'context-key-123abc'
+};
+const client = LDClient.initialize(
+  '6475b1b40b0fce12786b7091', 
+  context,
+  options = {
+      bootstrap: 'localStorage'
+    }
+  );
+
+
+client.on('ready', () => {
+  // initialization succeeded, flag values are now available
+  const flagValue = client.variation('flag-1', false);
+  console.log("Flag value: " + flagValue);
+  client.setStreaming(true); 
+
+  if(flagValue == true){
+    document.querySelector("#about-me").innerHTML = 'About Me (flag is true)';
+  }
+  if(flagValue == false){
+    document.querySelector("#about-me").innerHTML = 'About Me (flag is false)';
+
+  }
+});
